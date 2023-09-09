ca6a3725974ad625694d4a1934358b09c0c0f036
Rahul Dey <rahuldey9623@gmail.com>
2023-09-09 21:46:53 +0530

    Solved Buy and Sell Stock

--- best_time_to_buy_sell_stock.cpp
+++ best_time_to_buy_sell_stock.cpp
@@ -1,16 +1,25 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-void solve(int arr[], int n){
-	cout << n <<endl;
+int solve(int arr[], int n){
+	int mini = arr[0];
+	int maxProfit = 0;
+
+	for (int i = 1; i < n; ++i)
+	{
+		int cost = arr[i] - mini;
+		maxProfit = max(maxProfit, cost);
+		mini = min(mini, arr[i]);
+	}
+
+	return maxProfit;
 }
 
 int main() {
-	#ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
 	freopen("inputf.in","r",stdin); //file input.txt is opened in reading mode i.e "r"
 	freopen("outputf.out","w",stdout);  //file output.txt is opened in writing mode i.e "w"
-	#endif
+#endif
 
 	int n;
 	cin >> n;
@@ -20,7 +29,7 @@ int main() {
 		cin >> arr[i];
 	}
 
-	solve(arr, n);
-
+	cout << solve(arr, n) << endl;
+	
 	return 0;
 }
